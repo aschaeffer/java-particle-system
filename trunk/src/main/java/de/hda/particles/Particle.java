2d1537507a50bed6db5f944570a7fa2fdf688e7e
Peter.Penacka@gmail.com <Peter.Penacka@gmail.com@b00c6d9c-fa30-6ccc-9a85-475c53e0c171>
2012-10-27 17:24:51 +0000

    * created data structure of a single particle

--- trunk/src/main/java/de/hda/particles/Particle.java
+++ trunk/src/main/java/de/hda/particles/Particle.java
@@ -1,5 +1,73 @@
 package de.hda.particles;
 
+import java.util.Date;
+
 public class Particle {
+
+	Vector3 position = new Vector3();
+	Vector3 velocity = new Vector3();
+	long birth;
+	Integer lifetime;
+	
+	public Particle(Float p_x, Float p_y, Float p_z, Float v_x, Float v_y, Float v_z, Integer lifetime) {
+		this.position.x = p_x;
+		this.position.y = p_y;
+		this.position.z = p_z;
+		
+		this.velocity.x = v_x;
+		this.velocity.y = v_y;
+		this.velocity.z = v_z;
+		
+		this.birth = new Date().getTime();
+		this.lifetime = lifetime;
+	}
+
+	public void setPosition(Float x, Float y, Float z) {
+		position.x = x;
+		position.y = y;
+		position.z = z;
+	}
+	
+	public Float getX() {
+		return position.x;
+	}
+
+	public void setX(Float x) {
+		position.x = x;
+	}
+
+	public Float getY() {
+		return position.y;
+	}
+
+	public void setY(Float y) {
+		position.y = y;
+	}
+
+	public Float getZ() {
+		return position.z;
+	}
+
+	public void setZ(Float z) {
+		position.z = z;
+	}
+
+	public Vector3 getVelocity() {
+		return velocity;
+	}
+
+	public void setVelocity(Float x, Float y, Float z) {
+		velocity.x = x;
+		velocity.y = y;
+		velocity.z = z;
+	}
+
+	public void setVelocity(Vector3 velocity) {
+		this.velocity = velocity;
+	}
+
+	public Boolean isAlive() {
+		return (lifetime < new Date().getTime());
+	}
 
 }
---
+++ trunk/src/main/java/de/hda/particles/Vector3.java
@@ -0,0 +1,9 @@
+package de.hda.particles;
+
+public class Vector3 {
+
+	Float x = 0.0f;
+	Float y = 0.0f;
+	Float z = 0.0f;
+
+}
