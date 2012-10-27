366014538c8d19e7c173721ef96ae0bd92227637
Peter.Penacka@gmail.com <Peter.Penacka@gmail.com@b00c6d9c-fa30-6ccc-9a85-475c53e0c171>
2012-10-27 21:19:12 +0000

    * added method for automatically stop system updater after given time

--- trunk/src/main/java/de/hda/particles/SystemUpdater.java
+++ trunk/src/main/java/de/hda/particles/SystemUpdater.java
@@ -1,22 +1,31 @@
 package de.hda.particles;
 
+import java.util.Date;
+
 public class SystemUpdater implements Runnable {
 
 	private Updateable subject;
 	private Boolean running = true;
+	private long stopInMillis = 0;
 	
 	public SystemUpdater(Updateable subject) {
 		this.subject = subject;
 	}
 	
 	public void run() {
-		while(running) {
+		running = true;
+		while(running && stopInMillis > 0 && stopInMillis < new Date().getTime()) {
 			subject.update();
 		}
+		running = false;
 	}
 
 	public void stop() {
 		running = false;
+	}
+	
+	public void stopIn(Integer millis) {
+		stopInMillis = new Date().getTime() + millis;
 	}
 
 }
