579d5cbf14b1371d19bd89a8bfbb54e58a9d948b
Peter.Penacka@gmail.com <Peter.Penacka@gmail.com@b00c6d9c-fa30-6ccc-9a85-475c53e0c171>
2012-10-27 17:32:03 +0000

    added getters and setters for vector3

--- trunk/src/main/java/de/hda/particles/Vector3.java
+++ trunk/src/main/java/de/hda/particles/Vector3.java
@@ -2,8 +2,27 @@ package de.hda.particles;
 
 public class Vector3 {
 
-	Float x = 0.0f;
-	Float y = 0.0f;
-	Float z = 0.0f;
+	public Float x = 0.0f;
+	public Float y = 0.0f;
+	public Float z = 0.0f;
+
+	public Float getX() {
+		return x;
+	}
+	public void setX(Float x) {
+		this.x = x;
+	}
+	public Float getY() {
+		return y;
+	}
+	public void setY(Float y) {
+		this.y = y;
+	}
+	public Float getZ() {
+		return z;
+	}
+	public void setZ(Float z) {
+		this.z = z;
+	}
 
 }
