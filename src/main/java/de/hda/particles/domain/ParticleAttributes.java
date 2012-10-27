0e1041f8419ecb48b58d142605ebc15181568794
Peter.Penacka@gmail.com <Peter.Penacka@gmail.com@b00c6d9c-fa30-6ccc-9a85-475c53e0c171>
2012-10-27 21:13:27 +0000

    * added hash map specialization classes for particle attributes and modifier configurations

---
+++ src/main/java/de/hda/particles/domain/ParticleAttributes.java
@@ -0,0 +1,9 @@
+package de.hda.particles.domain;
+
+import java.util.HashMap;
+
+public class ParticleAttributes extends HashMap<String, Object> {
+
+	private static final long serialVersionUID = -3288616878715170036L;
+
+}
---
+++ src/main/java/de/hda/particles/domain/ParticleModifierConfiguration.java
@@ -0,0 +1,10 @@
+package de.hda.particles.domain;
+
+import java.util.HashMap;
+
+public class ParticleModifierConfiguration extends HashMap<String, Object> {
+
+	private static final long serialVersionUID = -2469636233684594893L;
+
+	public static ParticleModifierConfiguration EMPTY = new ParticleModifierConfiguration();
+}
