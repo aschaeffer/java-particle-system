a5a0935096cac0bed467fec01da7cb5224ec777a
Peter.Penacka@gmail.com <Peter.Penacka@gmail.com@b00c6d9c-fa30-6ccc-9a85-475c53e0c171>
2012-10-27 16:37:40 +0000

    initial commit

---
+++ trunk/src/main/java/de/hda/particles/App.java
@@ -0,0 +1,13 @@
+package de.hda.particles;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
