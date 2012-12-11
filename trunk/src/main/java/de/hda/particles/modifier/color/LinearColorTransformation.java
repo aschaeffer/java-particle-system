e8f8584d5832c5e05bbb9a5b2b2bb49cbd2ed61b
Peter.Penacka@gmail.com <Peter.Penacka@gmail.com@b00c6d9c-fa30-6ccc-9a85-475c53e0c171>
2012-12-11 01:03:43 +0000

    * reduced memory consumption

--- trunk/src/main/java/de/hda/particles/modifier/color/LinearColorTransformation.java
+++ trunk/src/main/java/de/hda/particles/modifier/color/LinearColorTransformation.java
@@ -10,18 +10,26 @@ import de.hda.particles.modifier.ParticleModifier;
 public class LinearColorTransformation extends AbstractParticleModifier implements ParticleModifier {
 
 	public LinearColorTransformation() {}
+	
+	private Color s;
+	private Color e;
+	private Float p = 0.0f;
+	private Integer r = 0;
+	private Integer g = 0;
+	private Integer b = 0;
+	private Integer a = 0;
 
 	@Override
 	public void update(Particle particle) {
-		Color s = (Color) particle.get(ParticleColor.START_COLOR);
+		s = (Color) particle.get(ParticleColor.START_COLOR);
 		if (s == null) return;
-		Color e = (Color) particle.get(ParticleColor.END_COLOR);
+		e = (Color) particle.get(ParticleColor.END_COLOR);
 		if (e == null) return;
-		Float p = particle.getLifetimePercent();
-		Integer r = (int) (p * e.getRed()   + (1.0f - p) * s.getRed());
-		Integer g = (int) (p * e.getGreen() + (1.0f - p) * s.getGreen());
-		Integer b = (int) (p * e.getBlue()  + (1.0f - p) * s.getBlue());
-		Integer a = (int) (p * e.getAlpha() + (1.0f - p) * s.getAlpha());
+		p = particle.getLifetimePercent();
+		r = (int) (p * e.getRed()   + (1.0f - p) * s.getRed());
+		g = (int) (p * e.getGreen() + (1.0f - p) * s.getGreen());
+		b = (int) (p * e.getBlue()  + (1.0f - p) * s.getBlue());
+		a = (int) (p * e.getAlpha() + (1.0f - p) * s.getAlpha());
 		particle.put(ParticleColor.CURRENT_COLOR, new Color(r, g, b, a));
 	}
 
