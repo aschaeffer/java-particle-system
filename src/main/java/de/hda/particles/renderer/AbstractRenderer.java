a7f16ee2f5a16259d4dfb220d14837a544f2ddf5
Peter.Penacka@gmail.com <Peter.Penacka@gmail.com@b00c6d9c-fa30-6ccc-9a85-475c53e0c171>
2012-12-11 00:57:05 +0000

    * added text overlay system * renderer manager manages modelview, projection and viewport buffers * render axis to infinity * axis text overlay * emitter text overlay

--- src/main/java/de/hda/particles/renderer/AbstractRenderer.java
+++ src/main/java/de/hda/particles/renderer/AbstractRenderer.java
@@ -1,6 +1,16 @@
 package de.hda.particles.renderer;
 
+import static org.lwjgl.opengl.GL11.*;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import org.lwjgl.util.glu.GLU;
 import org.lwjgl.util.vector.Vector3f;
+import org.newdawn.slick.UnicodeFont;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import de.hda.particles.scene.Scene;
 
@@ -8,6 +18,7 @@ public abstract class AbstractRenderer implements Renderer {
 
 	protected Scene scene;
 	protected Boolean visible = true;
+	protected UnicodeFont font;
 
 	@Override
 	public void setup() {
@@ -58,6 +69,45 @@ public abstract class AbstractRenderer implements Renderer {
 	@Override
 	public Boolean isVisible() {
 		return visible;
+	}
+	
+	public void renderTitle(Vector3f position, String title, Float maxDist, Boolean debug) {
+		if (!scene.getRendererManager().getTextOverlay()) return;
+		Vector3f cameraToObject = new Vector3f();
+		Vector3f.sub(position, scene.getCameraManager().getPosition(), cameraToObject);
+		if (Math.abs(cameraToObject.length()) > maxDist && maxDist > 0) return;
+
+        // store the current state of the renderer
+        glPushAttrib(GL_DEPTH_BUFFER_BIT | GL_ENABLE_BIT);
+        glPushMatrix();
+        glLoadIdentity();
+        glMatrixMode(GL_PROJECTION);
+        glPushMatrix();
+            
+        // now enter orthographic projection
+        glLoadIdentity();
+        glOrtho(0, scene.getWidth(), scene.getHeight(), 0, -1, 1);
+        glDisable(GL_DEPTH_TEST);
+        glDisable(GL_LIGHTING);
+        FloatBuffer result = ByteBuffer.allocateDirect(3*8).order(ByteOrder.nativeOrder()).asFloatBuffer();
+        GLU.gluProject(position.x, position.y, position.z, scene.getRendererManager().getModelViewMatrix(), scene.getRendererManager().getProjectionMatrix(), scene.getRendererManager().getViewport(), result);
+        result.rewind();
+        Float x = result.get();
+        Float y = result.get();
+        Float y2 = scene.getHeight() - y;
+        Float z = result.get();
+        if (z > 1.0f) return;
+        if (!debug) {
+            font.drawString(x, y2, title);
+        } else {
+            font.drawString(x, y2, title + "\nx:"+x+"\ny:"+y+"\ny2:"+y2+"\nz:"+z);
+        }
+		glEnable(GL_DEPTH_TEST);
+        glPopMatrix();
+        glMatrixMode(GL_MODELVIEW);
+        glPopMatrix();
+        glPopAttrib();
+
 	}
 
 }
