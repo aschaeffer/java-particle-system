32b242c2b5107b21d2a23859eedf5d4abf4d35ce
Peter.Penacka@gmail.com <Peter.Penacka@gmail.com@b00c6d9c-fa30-6ccc-9a85-475c53e0c171>
2012-12-12 21:06:22 +0000

    * cleanup and code quality

--- trunk/src/main/java/de/hda/particles/camera/FirstPersonCamera.java
+++ trunk/src/main/java/de/hda/particles/camera/FirstPersonCamera.java
@@ -8,26 +8,33 @@ import org.lwjgl.util.vector.Vector3f;
 import de.hda.particles.scene.Scene;
 
 public class FirstPersonCamera extends AbstractCamera implements Camera {
+	
+	private final static Integer MAX_MODES = 2;
+	private final static Integer DEFAULT_MODE = 1;
 
-	private float dx = 0.0f;
-	private float dy = 0.0f;
-	private float dt = 0.0f; // length of frame
+	private Float deltaX = 0.0f;
+	private Float deltaY = 0.0f;
+	private Integer deltaWheel = 0;
+	private Float deltaTime = 0.0f; // length of frame
 	private long lastTime = 0; // when the last frame was
 	private long time = 0;
+	private Float movementModifier = 1.0f;
 
-	float mouseSensitivity = 0.05f;
-	float movementSpeed = 20.0f; // move 10 units per second
+	private final Float mouseSensitivity = 0.05f;
+	private final Float movementSpeed = 20.0f; // move 10 units per second
 
 	public FirstPersonCamera() {}
 
 	// Constructor that takes the starting x, y, z location of the camera
 	public FirstPersonCamera(String name, Scene scene, Vector3f position) {
 		super(name, scene, position);
+		mode = DEFAULT_MODE;
 	}
 
 	// Constructor that takes the starting x, y, z location of the camera
 	public FirstPersonCamera(String name, Scene scene, Vector3f position, Float yaw, Float pitch, Float roll, Float fov) {
 		super(name, scene, position, yaw, pitch, roll, fov);
+		mode = DEFAULT_MODE;
 	}
 
 	// moves the camera forward relative to its current rotation (yaw)
@@ -42,14 +49,6 @@ public class FirstPersonCamera extends AbstractCamera implements Camera {
 				position.x += distance * (float) Math.sin(Math.toRadians(yaw));
 				position.y -= distance * (float) Math.sin(Math.toRadians(pitch));
 				position.z -= distance * (float) Math.cos(Math.toRadians(yaw));
-//				position.x += distance * new Double(Math.cos(Math.toRadians(yaw))*Math.cos(Math.toRadians(pitch))).floatValue();
-//				position.y += distance * new Double(Math.sin(Math.toRadians(yaw))*Math.cos(Math.toRadians(pitch))).floatValue();
-//				position.z -= distance * new Double(Math.sin(Math.toRadians(pitch))).floatValue();
-//				Vector3f nextPosition = new Vector3f();
-//				Vector3f directionVector = getDirectionVector();
-//				directionVector.scale(distance);
-//				Vector3f.add(getPosition(), directionVector, nextPosition);
-//				setPosition(nextPosition);
 				break;
 		}
 	}
@@ -66,25 +65,17 @@ public class FirstPersonCamera extends AbstractCamera implements Camera {
 				position.x -= distance * (float) Math.sin(Math.toRadians(yaw));
 				position.y += distance * (float) Math.sin(Math.toRadians(pitch));
 				position.z += distance * (float) Math.cos(Math.toRadians(yaw));
-//				position.x -= distance * new Double(Math.cos(Math.toRadians(yaw))*Math.cos(Math.toRadians(pitch))).floatValue();
-//				position.y -= distance * new Double(Math.sin(Math.toRadians(yaw))*Math.cos(Math.toRadians(pitch))).floatValue();
-//				position.z += distance * new Double(Math.sin(Math.toRadians(pitch))).floatValue();
-//				Vector3f nextPosition = new Vector3f();
-//				Vector3f directionVector = getDirectionVector();
-//				directionVector.scale(distance);
-//				Vector3f.sub(getPosition(), directionVector, nextPosition);
-//				setPosition(nextPosition);
 				break;
 		}
 	}
 
-	// strafes the camera left relitive to its current rotation (yaw)
+	// strafes the camera left relative to its current rotation (yaw)
 	public void strafeLeft(float distance) {
 		position.x += distance * (float) Math.sin(Math.toRadians(yaw - 90));
 		position.z -= distance * (float) Math.cos(Math.toRadians(yaw - 90));
 	}
 
-	// strafes the camera right relitive to its current rotation (yaw)
+	// strafes the camera right relative to its current rotation (yaw)
 	public void strafeRight(float distance) {
 		position.x += distance * (float) Math.sin(Math.toRadians(yaw + 90));
 		position.z -= distance * (float) Math.cos(Math.toRadians(yaw + 90));
@@ -101,26 +92,23 @@ public class FirstPersonCamera extends AbstractCamera implements Camera {
 	@Override
 	public void update() {
 		time = Sys.getTime();
-		dt = (time - lastTime) / 1000.0f;
+		deltaTime = (time - lastTime) / 1000.0f;
 		lastTime = time;
 
-		// distance in mouse movement from the last getDX() call.
-		dx = Mouse.getDX();
-		// distance in mouse movement from the last getDY() call.
-		dy = Mouse.getDY();
+		// distance in mouse movement from the last getDX() / getDY() call.
+		deltaX = new Integer(Mouse.getDX()).floatValue();
+		deltaY = new Integer(Mouse.getDY()).floatValue();
 
 		// control camera yaw from x movement from the mouse
-		this.yaw(dx * mouseSensitivity);
+		this.yaw(deltaX * mouseSensitivity);
 		// control camera pitch from y movement from the mouse
-		this.pitch(dy * mouseSensitivity);
-		
-		Keyboard.next();
+		this.pitch(deltaY * mouseSensitivity);
 
 		// when passing in the distance to move
 		// we times the movementSpeed with dt this is a time scale
 		// so if its a slow frame u move more then a fast frame
 		// so on a slow computer you move just as fast as on a fast computer
-		float movementModifier = 1.0f;
+		movementModifier = 1.0f;
 
 		if (Keyboard.isKeyDown(Keyboard.KEY_LCONTROL)) {
 			movementModifier = movementModifier * 10.0f;
@@ -129,32 +117,32 @@ public class FirstPersonCamera extends AbstractCamera implements Camera {
 			movementModifier = movementModifier * 10.0f;
 		}
 		if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
-			this.walkForward(movementModifier * movementSpeed * dt);
+			this.walkForward(movementModifier * movementSpeed * deltaTime);
 		}
 		if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
-			this.walkBackwards(movementModifier * movementSpeed * dt);
+			this.walkBackwards(movementModifier * movementSpeed * deltaTime);
 		}
 		if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
-			this.strafeLeft(movementModifier * movementSpeed * dt);
+			this.strafeLeft(movementModifier * movementSpeed * deltaTime);
 			this.roll(-1.0f);
 		}
 		if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
-			this.strafeRight(movementModifier * movementSpeed * dt);
+			this.strafeRight(movementModifier * movementSpeed * deltaTime);
 			this.roll(1.0f);
 		}
 
-		Integer dw = Mouse.getDWheel();
-		if (dw < 0) {
-			this.zoom(movementModifier * -dw / 60.0f);
-		} else if (dw > 0) {
-			this.zoom(movementModifier * -dw / 60.0f);
+		deltaWheel = Mouse.getDWheel();
+		if (deltaWheel < 0) {
+			this.zoom(movementModifier * -deltaWheel / 60.0f);
+		} else if (deltaWheel > 0) {
+			this.zoom(movementModifier * -deltaWheel / 60.0f);
 		}
 
 		// look through the camera before you draw anything
 		this.lookThrough();
 
 		// reduce roll
-		this.rollReduce();
+		this.reduceRoll();
 	}
 
 	@Override
@@ -170,18 +158,17 @@ public class FirstPersonCamera extends AbstractCamera implements Camera {
 	@Override
 	public void nextMode() {
 		mode++;
-		if (mode > 1) mode = 0;
+		if (mode > MAX_MODES - 1) mode = 0;
 	}
 	
 	@Override
 	public String getModeName() {
 		switch (mode) {
 			case 0:
+			default:
 				return "First Person Movement Mode";
 			case 1:
 				return "Fly Movement Mode";
-			default:
-				return "Default Mode";
 		}
 	}
 
