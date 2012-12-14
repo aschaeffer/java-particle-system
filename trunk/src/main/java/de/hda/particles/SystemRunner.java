68e2eb5fc2f7d792613960584e543cc6d86fcb4c
Peter.Penacka@gmail.com <Peter.Penacka@gmail.com@b00c6d9c-fa30-6ccc-9a85-475c53e0c171>
2012-12-14 00:23:36 +0000

    * api documentation

--- trunk/src/main/java/de/hda/particles/SystemRunner.java
+++ trunk/src/main/java/de/hda/particles/SystemRunner.java
@@ -8,14 +8,39 @@ import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * The SystemRunner is a mechanism to run systems
+ * implementing the Updateable interface in its own
+ * threads.
+ * 
+ * @author aschaeffer
+ *
+ */
 public class SystemRunner {
 
-	private Map<String, Updateable> updateables = new HashMap<String, Updateable>();
-	private Map<String, SystemUpdater> systemUpdaters = new HashMap<String, SystemUpdater>();
-	private Map<String, Thread> systemUpdaterThreads = new HashMap<String, Thread>();
+	/**
+	 * The list of managed systems.
+	 */
+	private final Map<String, Updateable> updateables = new HashMap<String, Updateable>();
 	
+	/**
+	 * The list of system updaters of the managed systems.
+	 */
+	private final Map<String, SystemUpdater> systemUpdaters = new HashMap<String, SystemUpdater>();
+	
+	/**
+	 * The list of system updater threads, that are running the managed systems.
+	 */
+	private final Map<String, Thread> systemUpdaterThreads = new HashMap<String, Thread>();
+
+	/**
+	 * True, if all systems are still running.
+	 */
 	private Boolean running = true;
 	
+	/**
+	 * The logger.
+	 */
 	private final Logger logger = LoggerFactory.getLogger(SystemRunner.class);
 
 	/**
@@ -32,16 +57,18 @@ public class SystemRunner {
 		systemUpdaterThreads.put(name, systemUpdaterThread);
 	}
 
+	/**
+	 * Starts all system updater threads and loops until
+	 * one system updater has been stopped.
+	 * @throws InterruptedException
+	 */
 	public void start() throws InterruptedException {
-
-		// start all system updater threads
 		Set<String> systemUpdaterThreadNames = systemUpdaterThreads.keySet();
 		Iterator<String> systemUpdaterThreadNamesIterator = systemUpdaterThreadNames.iterator();
 		while (systemUpdaterThreadNamesIterator.hasNext()) {
 			Thread systemUpdaterThread = systemUpdaterThreads.get(systemUpdaterThreadNamesIterator.next());
 			systemUpdaterThread.start();
 		}
-		
 		// loop until one system updater has been stopped
 		running = true;
 		while(running) { // !renderUpdater.isFinished() && !physicsUpdater.isFinished()) {
@@ -56,17 +83,14 @@ public class SystemRunner {
 			}
 			Thread.sleep(500);
 		}
-		
 		// stop all system updaters
 		Set<String> systemUpdaterNames = systemUpdaters.keySet();
 		Iterator<String> systemUpdaterNamesIterator = systemUpdaterNames.iterator();
 		while (systemUpdaterNamesIterator.hasNext()) {
 			systemUpdaters.get(systemUpdaterNamesIterator.next()).stop();
 		}
-		
-		// wait for
+		// wait for a half second
 		Thread.sleep(500);
-
 	}
 	
 }
