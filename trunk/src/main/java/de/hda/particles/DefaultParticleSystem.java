dfd4157831bd2e243c30ad1b2270bf52d08d0a90
Peter.Penacka@gmail.com <Peter.Penacka@gmail.com@b00c6d9c-fa30-6ccc-9a85-475c53e0c171>
2012-12-14 00:10:05 +0000

    * api documentation

--- trunk/src/main/java/de/hda/particles/DefaultParticleSystem.java
+++ trunk/src/main/java/de/hda/particles/DefaultParticleSystem.java
@@ -1,5 +1,11 @@
 package de.hda.particles;
 
+/**
+ * Minimalistic implementation of a particle system.
+ * 
+ * @author aschaeffer
+ *
+ */
 public class DefaultParticleSystem extends AbstractParticleSystem implements ParticleSystem {
 
 	public final static String SYSTEM_NAME = "particle physics";
--- trunk/src/main/java/de/hda/particles/ParticleLifetimeListener.java
+++ trunk/src/main/java/de/hda/particles/ParticleLifetimeListener.java
@@ -2,9 +2,25 @@ package de.hda.particles;
 
 import de.hda.particles.domain.Particle;
 
+/**
+ * Listener interface for particle lifetime changes. Informs
+ * listener about particle creation and particle death.
+ * 
+ * @author aschaeffer
+ *
+ */
 public interface ParticleLifetimeListener {
 
+	/**
+	 * Called on particle creation.
+	 * @param particle
+	 */
 	void onParticleCreation(Particle particle);
+	
+	/**
+	 * Called on particle death.
+	 * @param particle
+	 */
 	void onParticleDeath(Particle particle);
 
 }
