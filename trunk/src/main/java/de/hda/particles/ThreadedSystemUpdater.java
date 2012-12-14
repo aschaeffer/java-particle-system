f6b48b83b1113ea25fdc487f82860f0fb14d2866
Peter.Penacka@gmail.com <Peter.Penacka@gmail.com@b00c6d9c-fa30-6ccc-9a85-475c53e0c171>
2012-12-14 00:32:23 +0000

    * api documentation

--- trunk/src/main/java/de/hda/particles/ThreadedSystemUpdater.java
+++ trunk/src/main/java/de/hda/particles/ThreadedSystemUpdater.java
@@ -5,21 +5,60 @@ import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Default implementation of the SystemUpdater interface which is
+ * designed for use with threads.
+ * 
+ * @author aschaeffer
+ *
+ */
 public class ThreadedSystemUpdater implements SystemUpdater {
 
-	private Updateable subject;
+	/**
+	 * The subject to be managed.
+	 */
+	private final Updateable subject;
+	
+	/**
+	 * True, if the system updater is still running.
+	 */
 	private Boolean running = true;
+	
+	/**
+	 * Maximum iterations or 0 to disable. 
+	 */
 	private long stopInMillis = 0;
+	
+	/**
+	 * Number of iterations of the system updater.
+	 */
 	private Integer doneIterations = 0;
+	
+	/**
+	 * Name of the system updater.
+	 */
 	private String name = "SystemUpdater";
 	
+	/**
+	 * The logger.
+	 */
 	private final Logger logger = LoggerFactory.getLogger(ThreadedSystemUpdater.class);
 	
+	/**
+	 * Creates a new ThreadedSystemUpdater.
+	 * 
+	 * @param name Name of the system updater.
+	 * @param subject Updateable to be managed.
+	 */
 	public ThreadedSystemUpdater(String name, Updateable subject) {
 		this.subject = subject;
 		this.name = name;
 	}
-	
+
+	/**
+	 * System updater thread main loop.
+	 */
+	@Override
 	public void run() {
 		try {
 			running = true;
@@ -43,14 +82,26 @@ public class ThreadedSystemUpdater implements SystemUpdater {
 		}
 	}
 
+	/**
+	 * Method to stop the system updater externally.
+	 */
+	@Override
 	public void stop() {
 		running = false;
 	}
-	
+
+	/**
+	 * Stop the system updater after a number of iterations.
+	 */
+	@Override
 	public void stopIn(Integer millis) {
 		stopInMillis = (new Date().getTime()) + millis;
 	}
-	
+
+	/**
+	 * Returns true, if the system updater is finished.
+	 */
+	@Override
 	public Boolean isFinished() {
 		return !running;
 	}
