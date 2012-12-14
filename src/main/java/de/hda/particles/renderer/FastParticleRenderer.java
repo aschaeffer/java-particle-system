2bb92d05f0b22f7356754420fe6321c8298e0937
Peter.Penacka@gmail.com <Peter.Penacka@gmail.com@b00c6d9c-fa30-6ccc-9a85-475c53e0c171>
2012-12-14 03:35:30 +0000

    * cleanup / code quality

--- src/main/java/de/hda/particles/renderer/FastParticleRenderer.java
+++ src/main/java/de/hda/particles/renderer/FastParticleRenderer.java
@@ -8,14 +8,13 @@ import java.util.ListIterator;
 
 import de.hda.particles.domain.Particle;
 
-public class SimpleParticleRenderer extends AbstractRenderer implements Renderer {
+public class FastParticleRenderer extends AbstractRenderer implements Renderer {
 
-	public SimpleParticleRenderer() {}
+	public FastParticleRenderer() {}
 
 	@Override
 	public void update() {
 		glPushMatrix();
-		// glEnable(GL_POINT_SMOOTH);
 		glEnable(GL_BLEND);
 		glPointSize(5.0f);
 		glColor4f(0.8f, 0.8f, 0.8f, 0.3f);
@@ -27,7 +26,6 @@ public class SimpleParticleRenderer extends AbstractRenderer implements Renderer
 			Particle particle = pIterator.next();
 			if (particle != null) {
 				glVertex3f(particle.getX(), particle.getY(), particle.getZ());
-				// System.out.println("particle x:" + particle.getX() + " y:" + particle.getY() + " z:" + particle.getZ());
 			}
 		}
 		glEnd();
