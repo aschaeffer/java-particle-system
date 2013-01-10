18d571026d8e266d83ece7d8f3fbecef04472df4
Peter.Penacka@gmail.com <Peter.Penacka@gmail.com@b00c6d9c-fa30-6ccc-9a85-475c53e0c171>
2013-01-10 03:07:13 +0000

    * non-threaded fallback for loading skyboxes

--- src/main/java/de/hda/particles/textures/DeferredTextureLoaderCallback.java
+++ src/main/java/de/hda/particles/textures/DeferredTextureLoaderCallback.java
@@ -5,5 +5,7 @@ import org.newdawn.slick.opengl.Texture;
 public interface DeferredTextureLoaderCallback {
 
 	public void setTexture(String key, Texture object);
+	
+	public void reportTextureLoadingError(String key, String name);
 
 }
