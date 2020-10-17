6f7a242615cce158b0a103cb8fb7aa6467cf1a7a
Andreas Schaeffer <andreas.schaeffer@clavisit.com>
2020-10-17 20:12:38 +0200

    Added javadocs

--- src/main/java/de/hda/particles/domain/Identifiable.java
+++ src/main/java/de/hda/particles/domain/Identifiable.java
@@ -1,8 +1,20 @@
 package de.hda.particles.domain;
 
+/**
+ * This interface makes an object identifiable by id.
+ */
 public interface Identifiable {
 
+	/**
+	 * Returns the id of the object.
+	 * @return The id of the object.
+	 */
 	Integer getId();
+
+	/**
+	 * Sets the id of the object.
+	 * @param id The id of the object.
+	 */
 	void setId(Integer id);
 	
 }
